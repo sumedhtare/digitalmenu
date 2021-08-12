843c4080b1f5fa4876ac9323bd3421bd0a489172
LAPTOP-C145VJSE\USER <sumedh.tare@gmail.com>
2021-08-12 16:32:51 +0530

    firebase fix

--- src/App.js
+++ src/App.js
@@ -30,18 +30,6 @@ function App() {
 
 const [count, setCount] = useState(0)
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBRKL2yLsocLgIN22WzeZpnDhAZuugxFi8",
-  authDomain: "digital-menu-4310d.firebaseapp.com",
-  databaseURL: "https://digital-menu-4310d-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "digital-menu-4310d",
-  storageBucket: "digital-menu-4310d.appspot.com",
-  messagingSenderId: "531678763244",
-  appId: "1:531678763244:web:cf6866ceecc1fb5f59edc9"
-};
-
-firebase.initializeApp(firebaseConfig);
-
 useEffect(()=>{
   let staters = [
     {
