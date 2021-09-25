93a0a7c9449b1513ee14e0c8dbda84a2e9e4928d
LAPTOP-C145VJSE\USER <sumedh.tare@gmail.com>
2021-09-25 18:22:03 +0530

    UI changes

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -9,11 +9,25 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Front from '../frront'
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 'auto',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 const Home =()=>{
     const [shouldScan, setShouldScan] = useState(false)
     const [ data, setData ] = React.useState('Not Found');
     const history = useHistory()
-
     const handleData =(data)=>{
         console.log('handleData', data)
         setData(data.text)
@@ -21,7 +35,7 @@ const Home =()=>{
     }
 
     return (
-        <div>
+        <div >
            <Front />
            <div id="place-to-visit" style={{display:'flex', flexDirection:'row', justifyContent:'space-evenly', margin:'100px 0 100px 0'}}>
            {/* <h1>Home page</h1> */}
@@ -66,8 +80,16 @@ const Home =()=>{
         <Button onClick={()=>history.push('/menu?q=home')} size="small">Home Delivery</Button>
       </CardActions>
     </Card>
+ 
+           </div>
 
-            {shouldScan && <div>
+           <Modal
+            open={shouldScan}
+            onClose={() => setShouldScan(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+<Box sx={style}>
              <BarcodeScannerComponent
              width={300}
              height={300}
@@ -76,8 +98,9 @@ const Home =()=>{
              }}
            />
            <h3>{data}</h3>
-           </div>} 
-           </div>
+           </Box>
+
+        </Modal>
         </div>
     )
 
